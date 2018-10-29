242d3adffd5f1ef2c11b08c99347b2a12d139a61
shandygunawan <shandy.gunawan@rocketmail.com>
2018-10-29 19:27:11 +0700

    integrate server.c and rename client.c

--- sendfile.c
+++ sendfile.c
@@ -11,7 +11,45 @@
 #include "dataHandler.cpp"
 #include "slidingWindow.h"
 
+int checkInt(char* text){
+/* Checks if the string is a number or not */
+    int flag = 1;
+
+    for (int i = 0; i < strlen(text); i++){
+        if ((text[i] < 0x30) || (text[i] > 0x39)){
+            flag = 0;
+            break;
+        }
+    }
+
+    return flag;
+}
+
 int main(int argc, char *argv[]) {
+
+	if (argc != 5){     //Jumlah parameter tidak sesuai
+        printf("usage: %s <filename> <windowsize> <buffersize> <port>\n",argv[0]);
+        printf("  where parameters are:\n");
+        printf("  filename\t: the name of the output file.\n");
+        printf("  windowsize\t: advertised window size (Total packets can be sent in one stream)\n");
+        printf("  buffersize\t: maximum data can be send per frame (in bytes)\n");
+        printf("  port\t\t: port number of the incoming connection\n");
+        exit(0);
+    }
+
+    if (!(checkInt(argv[2]))){      // Window size bukan berupa angka
+        printf("ERROR Window size is not a number\n");
+        exit(0);
+    }
+    if (!(checkInt(argv[3]))){      // Buffer size bukan berupa angka
+        printf("ERROR Buffer size is not a number\n");
+        exit(0);
+    }
+    if (!(checkInt(argv[4]))){      // Port bukan berupa angka
+        printf("ERROR Invalid port\n");
+        exit(0);
+    }
+
 
 	// ARGUMENTS HANDLING
 	char* filename = argv[1];
@@ -47,5 +85,6 @@ int main(int argc, char *argv[]) {
 	client_address.sin_port = htons(dest_port);
 	client_address.sin_addr.s_addr = inet_addr(dest_IP);
 
+	printf("%s %d %d %s %d\n",filename,window_size,buffer_size,ip,port);
 	return 0;
 }
