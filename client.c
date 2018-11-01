1988f5a463095c761185632a359b1c92cefe1f35
kevinfernaldy <kevin.fernaldy@gmail.com>
2018-11-01 13:35:09 +0700

    Added socket support for client and server
    Current bugs:
    - Received string may be corrupted, possible packet loss

--- client.c
+++ client.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+  
+//#define PORT 8080 
+#define MAXBUFFER 1024 
 
 int checkInt(char* text){
 /* Checks if the string is a number or not */
@@ -16,7 +24,7 @@ int checkInt(char* text){
     return flag;
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     if (argc != 6){     // Jumlah parameter tidak sesuai
         printf("usage: %s <filename> <windowsize> <buffersize> <destination_ip> <destination_port>\n",argv[0]);
         printf("  where parameters are:\n");
@@ -49,4 +57,80 @@ int main(int argc, char** argv){
     int port = atoi(argv[5]);
 
     printf("%s %d %d %s %d\n",filename,window_size,buffer_size,ip,port);
-}
+    // Creating a socket for both process
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    // Creating child process
+    // Note: it is alright to fork instantly if there is no pointer to be copied
+    pid_t pid = fork();
+    if (pid < 0){   // Child process creation failed, printing the error and exit
+        fprintf(stderr,"\n");
+        exit(1);
+    } else if (pid == 0){   // Child process - Process to handle incoming transmission
+        char buffer[MAXBUFFER];
+        struct sockaddr_in server_address; 
+      
+        memset(&server_address, 0, sizeof(server_address)); 
+        
+        server_address.sin_family = AF_INET;            //IPV4
+        server_address.sin_port = htons(port);          //From specified port
+        server_address.sin_addr.s_addr = INADDR_ANY;
+
+        int received_data_length, dummy;    //r_d_l keeps the length of received data (NOT THE STRING)
+        for (;;){
+            //Receives the data and try to print it
+            received_data_length = recvfrom(sockfd, (char *)buffer, MAXBUFFER, MSG_WAITALL, (struct sockaddr *) &server_address, &dummy);
+            buffer[received_data_length] = '\0';
+            printf("Message from server : %s\n\n", buffer);
+
+            if (strcmp(buffer,"EXIT") == 0){    //If socket receives exit string, break out of loop and close everything
+                break;
+            }
+        }
+        close(sockfd);
+
+        _exit(0);
+    } else {    // Parent process - Process to handle sending transmission (sending frames)
+        struct sockaddr_in server_address; 
+      
+        memset(&server_address, 0, sizeof(server_address)); 
+           
+        server_address.sin_family = AF_INET;            //IPV4
+        server_address.sin_port = htons(port);          //Specified port
+        server_address.sin_addr.s_addr = INADDR_ANY;
+
+        FILE * fp;
+        char * buffer;
+        size_t string_length = 0;
+        ssize_t read_status;
+
+        fp = fopen("./test.txt","r");
+        for(;;){
+            // Gets daa from the file
+            read_status = getline(&buffer,&string_length,fp);
+
+            // If we're not EOF yet
+            if (read_status != -1){
+                // Sends the data
+                sendto(sockfd, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &server_address, sizeof(server_address));
+                printf("String sent\n"); 
+            } else {
+                // Send exit flag to the server
+                buffer = "EXIT";
+                sendto(sockfd, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &server_address, sizeof(server_address));
+                break;
+            }
+
+        }
+        fclose(fp);
+        close(sockfd);
+
+        exit(0);
+    }
+
+    return 0; 
+} 
--- server.c
+++ server.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+  
+//#define PORT 8080 
+#define MAXLINE 1024 
 
 int checkInt(char* text){
 /* Checks if the string is a number or not */
@@ -16,7 +24,7 @@ int checkInt(char* text){
     return flag;
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) { 
     if (argc != 5){     //Jumlah parameter tidak sesuai
         printf("usage: %s <filename> <windowsize> <buffersize> <port>\n",argv[0]);
         printf("  where parameters are:\n");
@@ -47,4 +55,52 @@ int main(int argc, char** argv){
     int port = atoi(argv[4]);
 
     printf("%s %d %d %d\n",filename,window_size,buffer_size,port);
-}
+
+    //Creating socket
+    int sockfd; 
+    struct sockaddr_in server_address, client_address; 
+    
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    memset(&server_address, 0, sizeof(server_address)); 
+    memset(&client_address, 0, sizeof(client_address)); 
+    
+    //NOTE: server_address is the address where ./server runs
+    server_address.sin_family = AF_INET;         //IPv4
+    server_address.sin_port = htons(port);          //From specified port
+    server_address.sin_addr.s_addr = INADDR_ANY;    //Accepts any connection
+      
+    // Bind the socket with the server address information
+    if (bind(sockfd, (const struct sockaddr *)&server_address, sizeof(server_address)) < 0){ 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE);
+    } 
+      
+    int received_data_length, dummy;
+    char buffer[MAXLINE];
+    for(;;){
+        //Receive the data transmitted by client
+        received_data_length = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &client_address, &dummy); 
+        buffer[received_data_length] = '\0';
+        printf("Message from client : %s\n", buffer); 
+
+        if (strcmp(buffer,"EXIT") == 0){
+            //Client sends EXIT flag, send EXIT flag back to client and terminate server
+            char * exit = "EXIT";
+
+            sendto(sockfd, (const char *)exit, strlen(exit), MSG_CONFIRM, (const struct sockaddr *) &client_address, sizeof(client_address));
+            break;
+        } else {
+            //Client not sending the EXIT flag, send response
+            char * response = "Message received";
+
+            sendto(sockfd, (const char *)response, strlen(response), MSG_CONFIRM, (const struct sockaddr *) &client_address, sizeof(client_address)); 
+            printf("Response sent.\n");
+        }
+    }
+      
+    return 0; 
+} 
